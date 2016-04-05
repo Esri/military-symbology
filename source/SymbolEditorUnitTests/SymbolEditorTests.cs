812ec0404c5a52e5109ac6f96e2cf71697bf50b4
Travis Lauver <tlauver@esri.com>
2016-04-05 13:59:52 -0400

    Changed UI, added Unit Tests
    Made image in modify tab not scale, so we could put a scrollviewer
    around it.  Added unit tests (which aren't quite working yet), and fixed
    a bug with the Echelon combo box not showing in the modify tab (Github
    issue #15)

---
+++ source/SymbolEditorUnitTests/SymbolEditorTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProSymbolEditor;
+using ArcGIS.Desktop.Mapping;
+using ArcGIS.Core.Geometry;
+
+namespace SymbolEditorUnitTests
+{
+    [TestClass]
+    public class SymbolEditorTests
+    {
+        [TestMethod]
+        public void CheckLayerForSymbolSetTest()
+        {
+            SymbolSetMappings symbolSetMappings = new SymbolSetMappings();
+            string featureClassName = symbolSetMappings.GetFeatureClassFromMapping("01", StyleItemType.PointSymbol);
+            Assert.IsTrue(featureClassName == "Air", "Feature Class from mapping is incorrect");
+
+            featureClassName = symbolSetMappings.GetFeatureClassFromMapping("40", StyleItemType.PointSymbol);
+            Assert.IsTrue(featureClassName == "Activities", "Feature Class from mapping is incorrect");
+
+            featureClassName = symbolSetMappings.GetFeatureClassFromMapping("60", StyleItemType.PointSymbol);
+            Assert.IsTrue(featureClassName == "Cyberspace", "Feature Class from mapping is incorrect");
+        }
+
+        [TestMethod]
+        public void CoordinateTypeTest()
+        {
+            MapPoint mapPoint;
+            var coordType = ProSymbolUtilities.GetCoordinateType("10SFF", out mapPoint);
+            Assert.IsTrue(mapPoint != null, "MGRS coordinate is invalid, when it should be valid");
+
+            coordType = ProSymbolUtilities.GetCoordinateType("blahblah", out mapPoint);
+            Assert.IsTrue(mapPoint == null, "MGRS coordinate is valid, when it should be invalid");
+        }
+    }
+}
