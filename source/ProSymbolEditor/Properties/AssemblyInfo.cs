9aabc5690ad1d9b95e49dd9bed6d0472ca0f013e
Chris Moore <csmooreacm@acm.org>
2017-02-13 20:03:40 -0500

    Run XAML Styler #130

--- source/ProSymbolEditor/Properties/AssemblyInfo.cs
+++ source/ProSymbolEditor/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ProSymbolEditor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MilitarySymbolEditor")]
+[assembly: AssemblyDescription("MilitarySymbolEditor")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Acme")]
-[assembly: AssemblyProduct("ProSymbolEditor")]
-[assembly: AssemblyCopyright("Copyright © Acme 2016")]
+[assembly: AssemblyCompany("Esri")]
+[assembly: AssemblyProduct("MilitarySymbolEditor")]
+[assembly: AssemblyCopyright("Copyright © Esri 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0")]
-[assembly: AssemblyFileVersion("1.4.0")]
+[assembly: AssemblyVersion("2.0.1")]
+[assembly: AssemblyFileVersion("2.0.1")]
