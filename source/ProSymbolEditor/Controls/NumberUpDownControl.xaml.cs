b91e90ee133ca87eafdedd0527a6b4e860fb214c
Joseph McCausland <jmccausland@esri.com>
2019-01-17 12:36:21 -0500

    fix namespace for controls

--- source/ProSymbolEditor/Controls/NumberUpDownControl.xaml.cs
+++ source/ProSymbolEditor/Controls/NumberUpDownControl.xaml.cs
@@ -2,7 +2,7 @@
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace WpfBuildUpDownControl
+namespace ProSymbolEditor.Controls
 {
     /// <summary>
     /// Interaction logic for NumberUpDownControl.xaml
